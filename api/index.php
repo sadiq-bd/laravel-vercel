8540ef4b2eba98ebaffea7ece29164182da50e61
Sadiq Ahmed <72698818+sadiq-bd@users.noreply.github.com>
2025-11-03 11:50:49 +0600

    Update index.php

--- api/index.php
+++ api/index.php
@@ -4,16 +4,6 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-// Check if the requested uri contains .php 
-if (preg_match('#[a-z0-9_-]+\.php(.*)#i', $_SERVER['REQUEST_URI'])) {
-	http_response_code(403);
-	header('Content-Type: application/json');
-	die(json_encode([
-        'status' => 'ERR',
-        'message' => 'Access Denied'
-    ], JSON_PRETTY_PRINT));
-}
-
 // Determine if the application is in maintenance mode...
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
