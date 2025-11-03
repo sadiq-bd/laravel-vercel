1adc555e59ddd2e4f9d1048204d3175834480d61
Sadiq Ahmed <72698818+sadiq-bd@users.noreply.github.com>
2025-11-03 11:49:56 +0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -9,11 +9,3 @@ Route::get('/', function () {
 Route::get('/phpinfo', function () {
     phpinfo();
 });
-
-// fallback json response
-Route::fallback(function () {
-    return response()->json([
-        'status' => 'ERR',
-        'message' => 'Access Denied',
-    ], 403, [], JSON_PRETTY_PRINT);
-});
